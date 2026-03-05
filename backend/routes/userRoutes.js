c9df3f37a5abf84c7289cf3dda9597c9c0b740aa
edhasingh125 <edha.singh.s.125@kalvium.community>
2026-03-05 15:28:03 +0530

    feat: implement user login API with JWT authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -28,6 +29,45 @@ router.post("/register",async(req,res)=>{
         });
     } catch(error){
         res.status(500).json({message: "Server error"});
+    }
+});
+
+// post /api/users/login
+router.post("/login", async (req,res)=>{
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            "secret123",
+            { expiresIn: "7d" }
+        );
+
+        res.json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({ message: "server error" });
     }
 });
 
